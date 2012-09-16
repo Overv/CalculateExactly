81953ef98fcfd12316680933dada2c02b8f9fe59
Alexander Overvoorde <overv161@gmail.com>
2012-09-16 02:38:41 +0200

    Added java file with base specification.

---
+++ CalculateExactly.java
@@ -0,0 +1,49 @@
+public class CalculateExactly {
+	/**
+	 * Returns the result of adding two real numbers.
+	 * @param a Left-hand value of addition operation
+	 * @param b Right-hand value of addition operation
+	 * @return Sum of the two values
+	 */
+	public char[] add(char[] a, char[] b) {
+		return null;
+	}
+
+	/**
+	 * Returns the result of subtracting two real numbers.
+	 * @param a Left-hand value of subtraction operation
+	 * @param b Right-hand value of subtraction operation
+	 * @return Result of subtracing b from a
+	 */
+	public char[] subtract(char[] a, char[] b) {
+		return null;
+	}
+
+	/**
+	 * Returns the result of multiplying two real numbers.
+	 * @param a Left-hand value of multiplication operation
+	 * @param b Right-hand value of multiplication operation
+	 * @return Product of the two values
+	 */
+	public char[] multiply(char[] a, char[] b) {
+		return null;
+	}
+
+	/**
+	 * Returns the result of dividing two real numbers.
+	 * @param a Left-hand value of division operation
+	 * @param b Right-hand value of division operation
+	 * @return Result of dividing a by b
+	 */
+	public char[] divide(char[] a, char[] b) {
+		return null;
+	}
+
+	/**
+	 * Sample program allowing a user to interact with exact calculations
+	 * and test the CalculateExactly implementation.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
