3a7c314e4a50b372e8cdc2ffb98597a86f1cadf8
Alexander Overvoorde <overv161@gmail.com>
2012-09-16 16:06:18 +0200

    Added number parsing function and finished basic test program.

--- CalculateExactly.java
+++ CalculateExactly.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CalculateExactly {
 	/**
 	 * Returns the result of adding two real numbers.
@@ -5,8 +7,8 @@ public class CalculateExactly {
 	 * @param b Right-hand value of addition operation
 	 * @return Sum of the two values
 	 */
-	public char[] add(char[] a, char[] b) {
-		return null;
+	public static char[] add(char[] a, char[] b) {
+		return "3.14".toCharArray();
 	}
 
 	/**
@@ -15,8 +17,8 @@ public class CalculateExactly {
 	 * @param b Right-hand value of subtraction operation
 	 * @return Result of subtracing b from a
 	 */
-	public char[] subtract(char[] a, char[] b) {
-		return null;
+	public static char[] subtract(char[] a, char[] b) {
+		return "3.14".toCharArray();
 	}
 
 	/**
@@ -25,8 +27,8 @@ public class CalculateExactly {
 	 * @param b Right-hand value of multiplication operation
 	 * @return Product of the two values
 	 */
-	public char[] multiply(char[] a, char[] b) {
-		return null;
+	public static char[] multiply(char[] a, char[] b) {
+		return "3.14".toCharArray();
 	}
 
 	/**
@@ -35,8 +37,32 @@ public class CalculateExactly {
 	 * @param b Right-hand value of division operation
 	 * @return Result of dividing a by b
 	 */
-	public char[] divide(char[] a, char[] b) {
-		return null;
+	public static char[] divide(char[] a, char[] b) {
+		return "3.14".toCharArray();
+	}
+
+	/**
+	 * Verifies that a string contains a valid real number
+	 * and parses it into a correct char array.
+	 */
+	public static char[] parseString(String str) {
+		str = str.trim();
+
+		// There is a sign character in the middle of the number
+		if (str.lastIndexOf('-') > 0) throw new NumberFormatException();
+
+		// There are multiple decimal periods
+		if (str.indexOf('.') != str.lastIndexOf('.')) throw new NumberFormatException();
+
+		char[] number = str.trim().toCharArray();
+
+		// Verify that there are no illegal characters
+		for (char c : number) {
+			if ((c < '0' || c > '9') && c != '.' && c != '-')
+				throw new NumberFormatException();
+		}
+
+		return number;
 	}
 
 	/**
@@ -44,6 +70,46 @@ public class CalculateExactly {
 	 * and test the CalculateExactly implementation.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		Scanner scan = new Scanner(System.in);
+
+		// Ask for operation
+		System.out.print("Operation (+, -, *, /): ");
+		String t = scan.nextLine();
+		if (!t.matches("[\\+\\-\\*\\/]")) {
+			System.out.println("Invalid operation specified.");
+			return;
+		}
+		char op = t.charAt(0);
+
+		// Ask for operands
+		char[] a = null;
+		char[] b = null;
+		try {
+			System.out.print("Left-hand operand: ");
+			a = parseString(scan.nextLine());
+			System.out.print("Right-hand operand: ");
+			b = parseString(scan.nextLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid number specified.");
+			return;
+		}
+
+		// Evaluate calculation
+		char[] r = null;
+		switch (op) {
+			case '+':
+				r = add(a, b);
+				break;
+			case '-':
+				r = subtract(a, b);
+				break;
+			case '*':
+				r = multiply(a, b);
+				break;
+			case '/':
+				r = divide(a, b);
+				break;
+		}
+		System.out.println(new String(a) + " " + op + " " + new String(b) + " = " + new String(r));
 	}
 }
